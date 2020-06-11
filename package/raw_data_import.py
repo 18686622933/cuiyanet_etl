f959132eb0bddc74b2e708ef893b824b0e25347b
陈博文 <48397447+18686622933@users.noreply.github.com>
2020-06-11 14:16:15 +0800

    Update raw_data_import.py

--- package/raw_data_import.py
+++ package/raw_data_import.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-
-# !/usr/bin/env python3
-# -*- coding:utf-8 -*-
-
 from package import mycuiyanet
 from package import base_data
 from package import execel_data
