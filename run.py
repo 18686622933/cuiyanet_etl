b1e740c50bcf1ea07f8d74b0f4c868d73740525d
陈博文 <48397447+18686622933@users.noreply.github.com>
2020-06-11 13:55:31 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -5,6 +5,7 @@
 from package import raw_data_import
 from package import config_file
 from package import sub_function
+from package import algo_data_export
 import pymysql
 
 while True:
@@ -14,10 +15,10 @@ while True:
         conn.close()
 
         if key == 1:
-            sub_function.db_connect_test(config_file.config)
+            # sub_function.db_connect_test(config_file.config)
             raw_data_import.data_import(config_file.config)
         elif key == 2:
-            print(2)
+            algo_data_export.aglo_data_export(config_file.config)
         elif key == 3:
             print(3)
         elif key == 4:
